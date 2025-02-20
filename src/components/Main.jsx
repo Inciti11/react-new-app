c85a439c320a04a5f6f535a45c4fd5a8250851e2
inciti11 <it@inciti.co>
2025-02-20 13:37:33 -0500

    Ajustes

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Constants from 'expo-constants'
+import { Text, View } from 'react-native'
+
+const Main = () => {
+    return (
+        <View style={ { marginTop: Constants}}>
+            <Text>VIEW MAPS</Text>
+        </View>
+    )
+}
+
+export default Main
